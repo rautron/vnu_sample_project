730de98dd15568ad30ec37f64d71383f4a833c7c
sanada.masashi <masashi.sanada@sun-asterisk.com>
2020-08-21 11:32:52 +0000

    rubyapp commit

---
+++ sample.rb
@@ -0,0 +1,19 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+
+  def get_total_price(count)
+    total_price = self.price * count
+    if count >= 2
+      total_price -= 10000
+    end
+    return "#{self.name} #{total_price}vnd"
+  end
+end
+
+menu1 = Menu.new
+menu1.name = "Phở"
+menu1.price = 30000
+
+puts menu1.get_total_price(2)
+
